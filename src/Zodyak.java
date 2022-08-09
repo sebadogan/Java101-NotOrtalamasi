6121dd2f5c2b0fd69b32ace84a45e041df2e47ab
sebadogan <doganseba@hotmail.com>
2022-08-09 18:03:28 +0300

    Zodyak

---
+++ src/Zodyak.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Zodyak {
+    public static void main(String[] args) {
+
+        int year ;
+        Scanner inp = new Scanner(System.in);
+
+        System.out.print("Dogum Yılınızı Giriniz:");
+        year = inp.nextInt();
+
+        int kalan= year%12;
+
+        switch (kalan) {
+            case 0:
+                System.out.println("Maymun");
+                break;
+            case 1:
+                System.out.println("Horoz");
+                break;
+            case 2:
+                System.out.println("Köpek");
+                break;
+            case 3:
+                System.out.println("Domuz");
+                break;
+            case 4:
+                System.out.println("Fare");
+                break;
+            case 5:
+                System.out.println("Öküz");
+                break;
+            case 6:
+                System.out.println("Kaplan");
+                break;
+            case 7:
+                System.out.println("Tavşan");
+                break;
+            case 8:
+                System.out.println("Ejdarha");
+                break;
+            case 9:
+                System.out.println("Yılan");
+                break;
+            case 10:
+                System.out.println("At");
+                break;
+            case 11:
+                System.out.println("Koyun");
+                break;
+
+
+        }
+    }
+}
